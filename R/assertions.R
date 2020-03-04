5cd82473de96b497a534939091ca5ef0b9b43714
be-marc <marcbecker@posteo.de>
2020-03-04 21:29:04 +0100

    Fix documentation

--- R/assertions.R
+++ R/assertions.R
@@ -19,9 +19,9 @@ assert_terminator = function(terminator) {
 }
 
 #' @export 
-#' @param x ([any])
-#' @param empty ([logical(1)])
-#' @param .var.name ([character(1)])
+#' @param x (any)
+#' @param empty (`logical(1)`)
+#' @param .var.name (`character(1)`)
 #' @rdname mlr_assertions
 assert_set = function(x, empty = TRUE, .var.name = vname(x)) {
   assert_character(x, min.len = as.integer(!empty), any.missing = FALSE, min.chars = 1L, unique = TRUE, .var.name = .var.name)
