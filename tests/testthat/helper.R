84fa99bcdeefcff285e928af7da31075e62eba5a
Jakob Richter <code@jakob-r.de>
2020-03-30 19:27:43 +0200

    multi-objective test

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -4,3 +4,6 @@ PS_2D = ParamSet$new(list(
 ))
 FUN_2D = function(x) sum(as.numeric(x)^2)
 OBJ_2D = Objective$new(fun = FUN_2D, domain = PS_2D)
+
+FUN_2D_2D = function(x) c(x[[1]]^2, -x[[2]]^2)
+OBJ_2D_2D = Objective$new(fun = FUN_2D_2D, domain = PS_2D, ydim = 2, minimize = c(TRUE, FALSE))
--- tests/testthat/test_TerminatorPerfReached.R
+++ tests/testthat/test_TerminatorPerfReached.R
@@ -7,3 +7,12 @@ test_that("TerminatorPerfReached works", {
   a = random_search(obj, term, batch_size = 1L)
   expect_equal(sum(a$data$y1 < 0.2), 1)
 })
+
+test_that("TerminatorPerfReached works for multi-objective", {
+  obj = OBJ_2D_2D
+  term = TerminatorPerfReached$new()
+  term$param_set$values$level = c(y1 = 0.2, y2 = -0.2)
+  a = random_search(obj, term, batch_size = 1L)
+  a$data[, both := y1 < 0.2 & y2 > -0.2]
+  expect_equal(sum(a$data$both), 1)
+})
