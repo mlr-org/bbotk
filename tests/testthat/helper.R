db437475e907e2b63bec2b9c78a9b432c8d41c1a
Jakob Richter <code@jakob-r.de>
2020-04-02 15:43:46 +0200

    fix multi-objective

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -10,15 +10,6 @@ FUN_2D = function(xs) {
 FUN_2D_CODOMAIN = ParamSet$new(list(ParamDbl$new("y", tags = "minimize")))
 OBJ_2D = ObjectiveRFun$new(fun = FUN_2D, domain = PS_2D)
 
-MAKE_INST_2D = function(term) {
-  if (is.integer(term)) {
-    tt = TerminatorEvals$new()
-    tt$param_set$values$n_evals = term
-    term = tt
-  }
-  OptimInstance$new(OBJ_2D, param_set = PS_2D, terminator = term)
-}
-
 
 # Simple 2D Function with trafo
 PS_2D_TRF = PS_2D$clone()
@@ -35,6 +26,28 @@ OBJ_2D_TRF = ObjectiveRFun$new(fun = FUN_2D_TRF, domain = PS_2D_TRF)
 FUN_2D_2D = function(xs) {
   list(y1 = xs[[1]]^2, y2 = -xs[[2]]^2)
 }
-OBJ_2D_2D = ObjectiveRFun$new(fun = FUN_2D_2D, domain = PS_2D, codomain = make_ps_reals(2))
+FUN_2D_2D_CODOMAIN = ParamSet$new(list(
+  ParamDbl$new("y1", tags = "minimize"),
+  ParamDbl$new("y2", tags = "maximize")
+))
 
+OBJ_2D_2D = ObjectiveRFun$new(fun = FUN_2D_2D, domain = PS_2D, codomain = FUN_2D_2D_CODOMAIN)
+
+# General Helper
+MAKE_INST = function(objective = OBJ_2D, param_set = PS_2D, terminator = 5L) {
+  if (is.integer(terminator)) {
+    tt = TerminatorEvals$new()
+    tt$param_set$values$n_evals = terminator
+    terminator = tt
+  }
+  OptimInstance$new(objective = objective, param_set = param_set, terminator = terminator)
+}
+
+MAKE_INST_2D = function(terminator) {
+  MAKE_INST(objective = OBJ_2D, param_set = PS_2D, terminator = terminator)
+}
+
+MAKE_INST_2D_2D = function(terminator) {
+ MAKE_INST(objective = OBJ_2D_2D, param_set = PS_2D, terminator = terminator)
+}
 
--- tests/testthat/test_TerminatorPerfReached.R
+++ tests/testthat/test_TerminatorPerfReached.R
@@ -6,14 +6,15 @@ test_that("TerminatorPerfReached works", {
   inst = MAKE_INST_2D(term)
   a = random_search(inst, batch_size = 1L)
   expect_equal(sum(a$data$y < 0.2), 1)
+  expect_true(tail(a$data$y,1) < 0.2)
 })
 
-# test_that("TerminatorPerfReached works for multi-objective", {
-#   obj = OBJ_2D_2D
-#   term = TerminatorPerfReached$new()
-#   term$param_set$values$level = c(y1 = 0.2, y2 = -0.2)
-#   obj$terminator = term
-#   a = random_search(obj, batch_size = 1L)
-#   a$data[, both := y1 <= 0.2 & y2 >= -0.2]
-#   expect_equal(sum(a$data$both), 1)
-# })
+test_that("TerminatorPerfReached works for multi-objective", {
+  term = TerminatorPerfReached$new()
+  term$param_set$values$level = c(y1 = 0.2, y2 = -0.2)
+  inst = MAKE_INST_2D_2D(term)
+  a = random_search(inst, batch_size = 1L)
+  a$data[, both := y1 <= 0.2 & y2 >= -0.2]
+  expect_equal(sum(a$data$both), 1)
+  expect_true(tail(a$data$both,1))
+})
