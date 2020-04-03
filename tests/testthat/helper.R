f7411d0d7fa1666938d4fa64a3b98a05b6a4858f
Jakob Richter <code@jakob-r.de>
2020-04-03 12:32:14 +0200

    test trafo

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -12,15 +12,14 @@ OBJ_2D = ObjectiveRFun$new(fun = FUN_2D, domain = PS_2D)
 
 
 # Simple 2D Function with trafo
-PS_2D_TRF = PS_2D$clone()
+PS_2D_TRF = ParamSet$new(list(
+  ParamDbl$new("x1", lower = -1, upper = 1),
+  ParamDbl$new("x2", lower = 1, upper = 3)
+))
 PS_2D_TRF$trafo = function(x, param_set) {
-  x$x2 + 2
+  x$x2 = x$x2 - 2
   return(x)
 }
-FUN_2D_TRF = function(xs) {
-  list(y = xs[[1]]^2 + (xs[[2]]-2)^2)
-}
-OBJ_2D_TRF = ObjectiveRFun$new(fun = FUN_2D_TRF, domain = PS_2D_TRF)
 
 # Multi-objecitve 2D->2D function
 FUN_2D_2D = function(xs) {
--- tests/testthat/test_OptimInstance.R
+++ tests/testthat/test_OptimInstance.R
@@ -1,26 +1,38 @@
 context("OptimInstance")
 
-test_that("OptimInstance", {
-  ObjectiveTestEval = R6Class("ObjectiveTestEval",
-    inherit = Objective,
-    public = list(
-      eval = function(xs) list(y = sum(as.numeric(xs))^2)
-    )
+ObjectiveTestEval = R6Class("ObjectiveTestEval",
+  inherit = Objective,
+  public = list(
+    eval = FUN_2D
   )
-  obj = ObjectiveTestEval$new(domain = PS_2D)
+)
+obj_test_eval = ObjectiveTestEval$new(domain = PS_2D)
+
+test_that("OptimInstance", {
   terminator = term("evals", n_evals = 20)
-  
-  inst = OptimInstance$new(objective = obj, param_set = PS_2D, terminator = terminator)
+  inst = OptimInstance$new(objective = obj_test_eval, param_set = PS_2D, terminator = terminator)
   expect_r6(inst$archive, "Archive")
   expect_data_table(inst$archive$data, nrows = 0L)
   expect_identical(inst$archive$n_evals, 0L)
   expect_identical(inst$archive$n_batch, 0L)
   expect_list(inst$result)
 
-  xdt = data.table(x1=list(1,2,3), x2=list(1,2,3))
+  xdt = data.table(x1=-1:1, x2=list(-1,0,1))
   inst$eval_batch(xdt)
   expect_data_table(inst$archive$data, nrows = 3L)
+  expect_equal(inst$archive$data$y, c(2,0,2))
   expect_identical(inst$archive$n_evals, 3L)
   expect_identical(inst$archive$n_batch, 1L)
   expect_list(inst$result)
+})
+
+test_that("OptimInstance works with trafos", {
+  terminator = term("evals", n_evals = 20)
+  inst = OptimInstance$new(objective = obj_test_eval, param_set = PS_2D_TRF, terminator = terminator)
+  expect_r6(inst$archive, "Archive")
+  xdt = data.table(x1=-1:1, x2=list(1,2,3))
+  inst$eval_batch(xdt)
+  expect_data_table(inst$archive$data, nrows = 3L)
+  expect_equal(inst$archive$data$y, c(2,0,2))
+  expect_equal(inst$archive$data$opt_x[[1]], list(x1 = -1, x2 = -1))
 })
