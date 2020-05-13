9548b291b27ed3f11bf9f0c401061ae39d2693e5
Jakob Richter <code@jakob-r.de>
2020-05-13 09:45:37 +0200

    Add tests for not persistent extras (Fixes #46)

--- tests/testthat/test_OptimInstance.R
+++ tests/testthat/test_OptimInstance.R
@@ -36,12 +36,25 @@ test_that("OptimInstance works with extras input", {
   expect_equal(inst$archive$data$opt_x[[1]], list(x1 = -1, x2 = -1))
   expect_subset(colnames(xdt), colnames(inst$archive$data))
   expect_equal(xdt, inst$archive$data[, colnames(xdt), with = FALSE])
+
+  # just add extras sometimes
+  xdt = data.table(x1 = -1:1, x2 = list(1, 2, 3), extra2 = as.list(letters[4:6]), extra3 = list(1:3, 2:4, 3:5))
+  inst$eval_batch(xdt)
+  expect_data_table(inst$archive$data, nrows = 6L)
+  expect_equal(inst$archive$data$y, c(2, 0, 2, 2, 0, 2))
+  expect_equal(xdt, inst$archive$data[4:6, colnames(xdt), with = FALSE])
+  expect_equal(inst$archive$data$extra3[1:3], list(NULL, NULL, NULL))
+  expect_equal(inst$archive$data$extra1[4:6], rep(NA_character_, 3))
 })
 
 test_that("OptimInstance works with extras output", {
   fun_extra = function(xs) {
     y = sum(as.numeric(xs)^2)
-    list(y = y, extra1 = runif(1), extra2 = list(a = runif(1), b = Sys.time()))
+    res = list(y = y, extra1 = runif(1), extra2 = list(a = runif(1), b = Sys.time()))
+    if (y > 10) { #sometimes add extras
+      res$extra3 = -y
+    }
+    return(res)
   }
   obj_extra = ObjectiveRFun$new(fun = fun_extra, domain = PS_2D, codomain = FUN_2D_CODOMAIN)
   inst = MAKE_INST(objective = obj_extra, search_space = PS_2D, terminator = 20L)
@@ -51,4 +64,7 @@ test_that("OptimInstance works with extras output", {
   expect_numeric(inst$archive$data$extra1, any.missing = FALSE, len = nrow(xdt))
   expect_list(inst$archive$data$extra2, len = nrow(xdt))
 
+  xdt = data.table(x1 = -1:1, x2 = c(-11,10,9))
+  inst$eval_batch(xdt)
+  expect_equal(inst$archive$data$extra3, c(NA, NA, NA, -122, -100, -82))
 })
