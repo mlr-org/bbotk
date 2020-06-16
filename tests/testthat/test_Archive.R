42f8bc8bab9d45a48fa9adbcdfc21b5a9f212083
Marc Becker <marcbecker@posteo.de>
2020-06-16 09:46:59 +0200

    Add best and clear unit tests

--- tests/testthat/test_Archive.R
+++ tests/testthat/test_Archive.R
@@ -12,7 +12,24 @@ test_that("Archive", {
   a$add_evals(xdt, xss_trafoed, ydt)
   expect_equal(a$n_evals, 1)
   expect_equal(a$data()$opt_x, xss_trafoed)
-  # FIXME: a$get_best(), a$clear()
+  a$clear()
+  expect_data_table(a$data(), nrows = 0)
+})
+
+test_that("Archive best works", {
+  a = Archive$new(PS_2D, FUN_2D_CODOMAIN)
+  xdt = data.table(x1 = c(0, 0.5), x2 = c(1, 1))
+  xss_trafoed = list(list(x1 = c(0, 0.5), x2 = c(1, 1)))
+  ydt = data.table(y = c(1, 0.25))
+  a$add_evals(xdt, xss_trafoed, ydt)
+  expect_equal(a$best()$y,0.25)
+
+  a = Archive$new(PS_2D, FUN_2D_2D_CODOMAIN)
+  xdt = data.table(x1 = c(-1,-1,-1), x2 = c(1, 0, -1))
+  xss_trafoed = list(list(x1 = -1, x2 = 1), list(x1 = -1, x2 = 0), list(x1 = -1, x2 = 1))
+  ydt = data.table(y1 = c(1, 1, 1), y2 = c(-1, 0, -1))
+  a$add_evals(xdt, xss_trafoed, ydt)
+  expect_equal(a$best()$y2, 0)
 })
 
 test_that("Archive on 1D problem works", {
