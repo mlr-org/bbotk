ddd16f68c04db88d2a82681a1b70b8a75ac41378
Marc Becker <marcbecker@posteo.de>
2020-06-18 11:26:55 +0200

    Fix result_x_search_space typo

--- R/OptimInstance.R
+++ R/OptimInstance.R
@@ -132,7 +132,7 @@ OptimInstance = R6Class("OptimInstance",
 
     #' @field result_x_seach_space ([data.table::data.table])\cr
     #'   x part of the result in the *search space*.
-    result_x_seach_space = function() {
+    result_x_search_space = function() {
       private$.result[, self$search_space$ids(), with = FALSE]
     },
 
