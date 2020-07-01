ecf9c978c104afba1efa855d36d35f0bedc620ee
Marc Becker <marcbecker@posteo.de>
2020-07-01 09:29:39 +0200

    Fix unit tests

--- tests/testthat/test_OptimizerNLoptr.R
+++ tests/testthat/test_OptimizerNLoptr.R
@@ -1,9 +1,8 @@
 context("OptimizerNLoptr")
 
 test_that("OptimizerNLoptr", {
-  param_set = list(x0 = 1, algorithm = "NLOPT_LN_BOBYQA", xtol_rel = -1,
-    xtol_abs = -1, ftol_rel = -1, ftol_abs = -1)
-  z = test_optimizer(OptimizerNLoptr$new(), param_set, n_dim = 1,
+  z = test_optimizer("nloptr", x0 = 1, algorithm = "NLOPT_LN_BOBYQA",
+    xtol_rel = -1, xtol_abs = -1, ftol_rel = -1, ftol_abs = -1, n_dim = 1,
     term_evals = 5L)
   expect_class(z$optimizer, "OptimizerNLoptr")
   expect_output(print(z$optimizer), "OptimizerNLoptr")
