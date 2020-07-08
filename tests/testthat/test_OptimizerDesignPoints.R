6b3b991d8f0cae3a0a5629b7a399702f6ba75a2f
Marc Becker <marcbecker@posteo.de>
2020-07-08 10:53:29 +0200

    Use check_dt instead of Design to check validity of design in OptimizerDesignPoints

--- tests/testthat/test_OptimizerDesignPoints.R
+++ tests/testthat/test_OptimizerDesignPoints.R
@@ -13,4 +13,8 @@ test_that("OptimizerDesignPoints", {
 
   expect_error(test_optimizer("design_points", n_dim = 1, term_evals = 10L, real_evals = 3),
     "Please set design datatable")
+
+  design = data.table(x1 = c(2), x2 = c(2))
+  expect_error(test_optimizer("design_points", design = design, n_dim = 2, term_evals = 10L, real_evals = 3),
+    fixed = "Assertion on 'pv$design' failed: x1: Element 1 is not <= 1")
 })
