9e8da848ee12fd454dca6e23e44bf59adee25a45
Marc Becker <marcbecker@posteo.de>
2021-05-04 17:57:25 +0200

    bb_optimize

--- R/sugar.R
+++ R/sugar.R
@@ -66,3 +66,63 @@ opt = function(.key, ...) {
 opts = function(.keys, ...) {
   dictionary_sugar_mget(mlr_optimizers, .keys, ...)
 }
+
+
+#' @export
+bb_optimize = function(method, fun = NULL, objective = NULL, term_evals = NULL, term_time = NULL, search_space = NULL,
+  lower = NULL, upper = NULL, codomain = NULL, ...) {
+
+  assert_choice(method, mlr_optimizers$keys())
+  optimizer = opt(method, ...)
+  assert_function(fun, null.ok = TRUE)
+  assert_r6(objective, "Objective", null.ok = TRUE)
+  terminator = terminator_selection(term_evals, term_time)
+  if (!is.null(search_space)) assert_param_set(search_space)
+  assert_numeric(lower, null.ok = TRUE)
+  assert_numeric(upper, null.ok = TRUE)
+
+  if (!xor(is.null(fun), is.null(objective))) {
+    stop("Either `fun` or `objective` must be provided.")
+  }
+
+  if (xor(is.null(lower), is.null(upper))) {
+    stop("`lower` and `upper` must be provided.")
+  }
+
+  if (!is.null(fun)) {
+    if(is.null(search_space)) {
+      ids = if (is.null(names(lower))) paste0("x", seq(lower)) else names(lower)
+
+      search_space = ParamSet$new(pmap(list(ids, lower, upper), function(s, lb, ub) {
+        ParamDbl$new(id = s, lower = lb, upper = ub)
+      }))
+    }
+    if (is.null(codomain)) codomain = ps(y = p_dbl(tags = "minimize"))
+    objective = ObjectiveRFun$new(fun, search_space, codomain, check_values = FALSE)
+  }
+
+  instance = OptimInstanceSingleCrit$new(objective, terminator = terminator, check_values = FALSE)
+  optimizer$optimize(instance)
+  par = if (instance$search_space$all_numeric) {
+    set_names(as.numeric(instance$result_x_search_space), instance$search_space$ids()) 
+  } else {
+    instance$result_x_search_space
+  }
+
+  list(par =  par, value = instance$result_y, instance = instance)
+}
+
+terminator_selection = function(term_evals, term_time) {
+  assert_int(term_evals, null.ok = TRUE)
+  assert_int(term_time, null.ok = TRUE)
+
+  if (is.null(term_evals) && is.null(term_time)) {
+    trm("none")
+  } else if (!is.null(term_evals) && !is.null(term_time)) {
+    trm("combo", list(trm("evals", n_evals = term_evals), trm("run_time", secs = term_time)))
+  } else if (!is.null(term_evals)) {
+    trm("evals", n_evals = term_evals)
+  } else if (!is.null(term_time)) {
+    trm("run_time", secs = term_time)
+  }
+}
---
+++ tests/testthat/test_bb_optimize.R
@@ -0,0 +1,107 @@
+test_that("bb_optimize works with function and bounds", {
+  lower = c(-10, -5)
+  upper = c(10, 5)
+
+  fun = function(xs) {
+    - (xs[[1]] - 2)^2 - (xs[[2]] + 3)^2 + 10
+  }
+
+  res = bb_optimize(method = "random_search", fun = fun, lower = lower, upper = upper, term_evals = 10)
+  expect_list(res)
+  expect_numeric(res$par)
+  expect_named(res$par, c("x1", "x2"))
+  expect_numeric(res$value)
+  expect_named(res$value, "y")
+  expect_r6(res$instance, "OptimInstanceSingleCrit")
+})
+
+test_that("bb_optimize works with function and named bounds", {
+  lower = c(z1 = -10, z2 = -5)
+  upper = c(z1 = 10, z2 = 5)
+
+  fun = function(xs) {
+    - (xs[[1]] - 2)^2 - (xs[[2]] + 3)^2 + 10
+  }
+
+  res = bb_optimize(method = "random_search", fun = fun, lower = lower, upper = upper, term_evals = 10)
+  expect_list(res)
+  expect_numeric(res$par)
+  expect_named(res$par, c("z1", "z2"))
+  expect_numeric(res$value)
+  expect_named(res$value, "y")
+  expect_r6(res$instance, "OptimInstanceSingleCrit")
+})
+
+test_that("bb_optimize works with function and search space", {
+  search_space = ps(
+    x1 = p_dbl(-10, 10),
+    x2 = p_dbl(-5, 5)
+  )
+
+  fun = function(xs) {
+    - (xs[[1]] - 2)^2 - (xs[[2]] + 3)^2 + 10
+  }
+
+  res = bb_optimize(method = "random_search", fun = fun, search_space = search_space, term_evals = 10)
+  expect_list(res)
+  expect_numeric(res$par)
+  expect_named(res$par, c("x1", "x2"))
+  expect_numeric(res$value)
+  expect_named(res$value, "y")
+  expect_r6(res$instance, "OptimInstanceSingleCrit")
+})
+
+test_that("bb_optimize works with function, bounds and codomain", {
+  search_space = ps(
+    x1 = p_dbl(-10, 10),
+    x2 = p_dbl(-5, 5)
+  )
+
+  fun = function(xs) {
+    - (xs[[1]] - 2)^2 - (xs[[2]] + 3)^2 + 10
+  }
+
+  codomain = ps(z = p_dbl(tags = "minimize"))
+
+  objective = ObjectiveRFun$new(fun, search_space, codomain)
+
+  res = bb_optimize(method = "random_search", objective = objective, term_evals = 10)
+  expect_list(res)
+  expect_numeric(res$par)
+  expect_named(res$par, c("x1", "x2"))
+  expect_numeric(res$value)
+  expect_named(res$value, "z")
+  expect_r6(res$instance, "OptimInstanceSingleCrit")
+})
+
+test_that("bb_optimize works with objective", {
+  fun = function(xs) {
+    c(z = - (xs[[1]] - 2)^2 - (xs[[2]] + 3)^2 + 10)
+  }
+  search_space = ps(
+    x1 = p_dbl(-10, 10),
+    x2 = p_dbl(-5, 5)
+  )
+  codomain = ps(z = p_dbl(tags = "minimize"))
+  objective = ObjectiveRFun$new(fun, search_space, codomain) 
+
+  res = bb_optimize(method = "random_search", objective = objective, term_evals = 10)
+  expect_list(res)
+  expect_numeric(res$par)
+  expect_named(res$par, c("x1", "x2"))
+  expect_numeric(res$value)
+  expect_named(res$value, "z")
+  expect_r6(res$instance, "OptimInstanceSingleCrit")
+})
+
+test_that("bb_optimize checks work", {
+  lower = c(-10, -5)
+
+  fun = function(xs) {
+    - (xs[[1]] - 2)^2 - (xs[[2]] + 3)^2 + 10
+  }
+
+  res = bb_optimize(method = "random_search", fun = fun, lower = lower, term_evals = 10)
+
+})
+
