9c1c3717dc39d57263d2876fee1378f99b6ca615
Marc Becker <marcbecker@posteo.de>
2021-07-01 12:58:26 +0200

    update docs

--- R/OptimizerIrace.R
+++ R/OptimizerIrace.R
@@ -4,10 +4,10 @@
 #' @name mlr_optimizers_irace
 #'
 #' @description
-#' `OptimizerIrace` class that implements iterated racing. 
-#' Calls [irace::irace()] from package \CRANpkg{irace}.
-#' 
-#' #' @section Parameters:
+#' `OptimizerIrace` class that implements iterated racing. Calls
+#' [irace::irace()] from package \CRANpkg{irace}.
+#'  
+#' @section Parameters:
 #' \describe{
 #' \item{`instances`}{`list()`\cr
 #' A list of instances where the target algorithm is executed on.}
@@ -15,12 +15,45 @@
 #' A function that executes the target algorithm with a specific parameter configuration and instance.}
 #' }
 #'
-#' For the meaning of all other parameters, see [irace::defaultScenario()]. 
-#' Note that we have removed all control parameters which refer to the termination of
+#' For the meaning of all other parameters, see [irace::defaultScenario()]. Note
+#' that we have removed all control parameters which refer to the termination of
 #' the algorithm. Use [TerminatorRunTime] or [TerminatorEvals] instead. Other
-#' terminators do not work with `TunerIrace`. We substract 5 seconds from the
+#' terminators do not work with `TunerIrace`. We subtract 5 seconds from the
 #' [TerminatorRunTime] budget for stability reasons.
-#'
+#' 
+#' @section Target Runner and Instances:
+#' The irace package uses a `targetRunner` script or R function to evaluate a
+#' configuration on a particular instance. Usually it is not necessary to
+#' specify a `targetRunner` function when using `OptimizerIrace`. A default
+#' function is used that forwards several configurations and an instance to the
+#' user defined objective function. As usual, the configurations are passed in
+#' `xss` to the objective. The instance is passed via the `$irace_instance`
+#' public field. 
+#' 
+#' ```
+#' ObjectiveIrace = R6Class("ObjectiveIrace", inherit = Objective,
+#'   public = list(
+#'     irace_instance = NULL
+#'   ),
+#' 
+#'   private = list(
+#'     .eval_many = function(xss) {
+#'       # objective function to evaluate configurations in xss
+#'       # access irace instance via self$irace_instance
+#'     }
+#'   )
+#' )
+#' ```
+#' 
+#' Alternatively, `$.eval(xs)` can be implemented which only passes one
+#' configuration at once. The use of `$.eval_many(xss)` allows us to implement
+#' a parallelization e.g. with the \CRANpkg{future} package.
+#' 
+#' @section Result:
+#' The optimization result in `instance$result` is the best performing elite of
+#' the final race. The reported performance is the average performance estimated
+#' on all used instances.
+#' 
 #' @templateVar id irace
 #' @template section_dictionary_optimizers
 #'
@@ -44,7 +77,7 @@
 #'   ),
 #' 
 #'   private = list(
-#'     .eval = function(xs) {
+#'     .eval_many = function(xss) {
 #'       # branin function with `tau` noise 
 #'       a = 1
 #'       b = 5.1 / (4 * (pi ^ 2))
@@ -52,17 +85,20 @@
 #'       r = 6
 #'       s = 10
 #'       t = 1 / (8 * pi)
-#'       tau = self$irace_instance
-#' 
-#'       list(y = a * ((xs$x2 -
-#'       b * (xs$x1 ^ 2L) +
-#'       c * xs$x1 - r) ^ 2) +
-#'       ((s * (1 - t)) * cos(xs$x1)))
+#'  
+#'       data.table(y = mapply(function(xs, tau) {
+#'        a * ((xs$x2 -
+#'        b * (xs$x1 ^ 2) +
+#'        c * xs$x1 - r) ^ 2) +
+#'        ((s * (1 - t)) * cos(xs$x1)) +
+#'        tau
+#'       }, xss, self$irace_instance))
 #'     }
 #'   )
 #' )
 #' 
-#' objective = ObjectiveIrace$new(domain = domain,  codomain = ps(y = p_dbl(tags = "minimize")))
+#' codomain = ps(y = p_dbl(tags = "minimize"))
+#' objective = ObjectiveIrace$new(domain = domain,  codomain = codomain)
 #' 
 #' instance = OptimInstanceSingleCrit$new(
 #'   objective = objective,
@@ -73,14 +109,14 @@
 #' irace_instance = rnorm(10, mean = 0, sd = 0.1)
 #' optimizer = opt("irace", instances = irace_instance)
 #' 
-#'  # modifies the instance by reference
-#'  optimizer$optimize(instance)
+#' # modifies the instance by reference
+#' optimizer$optimize(instance)
 #' 
-#'  # best scoring evaluation
-#'  instance$result
+#' # best scoring evaluation
+#' instance$result
 #' 
-#'  # all evaluations
-#'  as.data.table(instance$archive)
+#' # all evaluations
+#' as.data.table(instance$archive)
 OptimizerIrace = R6Class("OptimizerIrace",
   inherit = Optimizer,
   public = list(
@@ -161,7 +197,7 @@ OptimizerIrace = R6Class("OptimizerIrace",
     # The reported performance value is the average of all resampling iterations.
     .assign_result = function(inst) {
         if(length(private$.result_id) == 0) {
-          stop("irace::irace did not return a result. The evaluated configurations are still accessible through the archive.")
+          stop("`irace::irace` did not return a result. The evaluated configurations are still accessible through the archive.")
         }
 
         res = inst$archive$data[get("id_configuration") == private$.result_id, ]
