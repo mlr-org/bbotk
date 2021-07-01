2cbe9277d85328b91bc260ab682a9f21807cae47
Marc Becker <marcbecker@posteo.de>
2021-07-01 13:05:14 +0200

    update helper

--- R/helper_irace.R
+++ R/helper_irace.R
@@ -1,30 +1,26 @@
-paradox_to_irace = function(ps) {
+paradox_to_irace = function(param_set) {
+  assertClass(param_set, "ParamSet")
+  if("ParamUty" %in% param_set$class) stop("<ParamUty> not supported by <TunerIrace>")
 
-  assertClass(ps, "ParamSet")
-  if("ParamUty" %in% ps$class) {
-    stop("<ParamUty> not supported by <TunerIrace>")
-  }
-
-  # what about ParamUty = vector numeric/real
   class_lookup = data.table(
     paradox = c("ParamLgl", "ParamInt", "ParamDbl", "ParamFct"),
     irace = c("c", "i", "r", "c"), stringsAsFactors = FALSE)
 
-  type = unlist(subset(merge(data.table(paradox = ps$class), class_lookup, sort = FALSE), select = "irace"))
-  range = get_irace_range(ps)
-  if (ps$has_deps) {
-    condition = get_irace_condition(ps)
+  type = unlist(subset(merge(data.table(paradox = param_set$class), class_lookup, sort = FALSE), select = "irace"))
+  range = get_irace_range(param_set)
+  if (param_set$has_deps) {
+    condition = get_irace_condition(param_set)
   } else {
     condition = NULL
   }
 
-  par_tab = paste(ps$ids(), '""', type, range, condition$cond, collapse = "\n")
+  par_tab = paste(param_set$ids(), '""', type, range, condition$cond, collapse = "\n")
 
   return(irace::readParameters(text = par_tab))
 }
 
-get_irace_range = function(ps) {
-  rng = data.table(lower = ps$lower, upper = ps$upper, lvl = ps$levels)
+get_irace_range = function(param_set) {
+  rng = data.table(lower = param_set$lower, upper = param_set$upper, lvl = param_set$levels)
 
   apply(rng, 1, function(x) {
     if (is.na(x[[1]])) {
@@ -35,8 +31,8 @@ get_irace_range = function(ps) {
   })
 }
 
-get_irace_condition = function(ps) {
-  cond = rbindlist(apply(ps$deps, 1, function(x) {
+get_irace_condition = function(param_set) {
+  cond = rbindlist(apply(param_set$deps, 1, function(x) {
     on = x[[2]]
     cond = x[[3]]$rhs
     if (is.character(cond)) {
@@ -51,13 +47,13 @@ get_irace_condition = function(ps) {
   }))
 
   # coercion back and forth from frame/table is due to data.frame sorting even when sort = FALSE
-  tab = data.frame(merge(data.table(id = ps$ids()), cond, by = "id", all.x = TRUE, sort = FALSE))
+  tab = data.frame(merge(data.table(id = param_set$ids()), cond, by = "id", all.x = TRUE, sort = FALSE))
   tab[is.na(tab)] = ""
 
   return(tab)
 }
 
-target_runner_default = function(experiment, exec.target.runner, scenario, target.runner) {  # nolint
+target_runner_default = function(experiment, exec.target.runner, scenario, target.runner) {# nolint
   optim_instance = scenario$targetRunnerData$inst
 
   # fix logicals
@@ -67,8 +63,7 @@ target_runner_default = function(experiment, exec.target.runner, scenario, targe
         return(as.logical(x))
       } else {
         return(x)
-      }
-  }))})
+      }}))})
 
   # provide experiment instance to objective
   optim_instance$objective$irace_instance = map(experiment, function(e) e$instance)
