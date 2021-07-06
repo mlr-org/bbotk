0334ad47a2b8c4aaae423d1c1069aae30ed880fb
Marc Becker <marcbecker@posteo.de>
2021-07-06 16:27:20 +0200

    fix unit test warning

--- tests/testthat/test_OptimizerCmaes.R
+++ tests/testthat/test_OptimizerCmaes.R
@@ -1,7 +1,29 @@
 test_that("OptimizerCmaes", {
   skip_if_not_installed("adagio")
 
-  z = test_optimizer_1d("cmaes", term_evals = 2L)
+  search_space = domain = ps(
+    x1 = p_dbl(-10, 10),
+    x2 = p_dbl(-5, 5)
+  )
+
+  codomain = ps(y = p_dbl(tags = "maximize"))
+
+  objective_function = function(xs) {
+    c(y = - (xs[[1]] - 2)^2 - (xs[[2]] + 3)^2 + 10)
+  }
+
+  objective = ObjectiveRFun$new(
+    fun = objective_function,
+    domain = domain,
+    codomain = codomain)
+
+  instance = OptimInstanceSingleCrit$new(
+    objective = objective,
+    search_space = search_space,
+    terminator = trm("evals", n_evals = 10))
+
+  z = test_optimizer(instance, "cmaes", real_evals = 10L)
+
   expect_class(z$optimizer, "OptimizerCmaes")
   expect_output(print(z$optimizer), "OptimizerCmaes")
 
