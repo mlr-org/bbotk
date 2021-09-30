9e0502e07c3907d935cb538d4270f9998853e99a
Marc Becker <marcbecker@posteo.de>
2021-09-30 16:41:26 +0200

    enhance unit tet

--- tests/testthat/test_nds_selection.R
+++ tests/testthat/test_nds_selection.R
@@ -36,6 +36,7 @@ test_that("nds_selection works", {
     list(c(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L))
   )
 
+  # default
   s2 = map(map(seq_len(8),function(i) replicate(100, nds_selection(points, n_select = i), simplify = FALSE)), unique)
 
   pmap_lgl(list(s1, s2), function(ss1, ss2) {
@@ -79,7 +80,7 @@ test_that("nds_selection in Archive works", {
   )
   a = Archive$new(domain, codomain)
 
-  xdt = data.table(x1 = as.numeric(seq_len(8)))
+  xdt = data.table(x1 = seq_len(8))
   ydt = data.table(
     y1 = c(1, 2, 3.9, 4, 2.2, 4, 4.2, 6),
     y2 = c(4, 2, 1.1, 1, 3.2, 3, 1, 6)
@@ -89,22 +90,24 @@ test_that("nds_selection in Archive works", {
 
   s1 = list(
     # Point 3 is ommitted first, followed by point 2. Then, 1 or 4 survives randomly.
-    list(1, 4),
+    list(1L, 4L),
     # Point 3 is ommitted first, followed by point 2. 1 and 4 survive both
-    list(c(1, 4)),
+    list(c(1L, 4L)),
     # Point 3 is ommited first, so points 1, 2, and 4 survive
-    list(c(1, 2, 4)),
+    list(c(1L, 2L, 4L)),
     # All points out of front 1 survive
-    list(c(1, 2, 3, 4)),
+    list(c(1L, 2L, 3L, 4L)),
     # Out of front 2, points 5 is ommitted first, then, either 5 or 7 are sampled randomly
-    list(c(1, 2, 3, 4, 5), c(1, 2, 3, 4, 7)),
+    list(c(1L, 2L, 3L, 4L, 5L), c(1L, 2L, 3L, 4L, 7L)),
     # Out of front 2, points 5 is ommitted first, and 5 and 7 survive
-    list(c(1, 2, 3, 4, 5, 7)),
+    list(c(1L, 2L, 3L, 4L, 5L, 7L)),
     # Whole front 2 survives
-    list(c(1, 2, 3, 4, 5, 6, 7)),
+    list(c(1L, 2L, 3L, 4L, 5L, 6L, 7L)),
     # all candidates survive
-    list(c(1, 2, 3, 4, 5, 6, 7, 8))
+    list(c(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L))
   )
+
+  # default
   s2 = map(map(seq_len(8),function(i) replicate(100, a$nds_selection(n_select = i)$x1,simplify = FALSE)), unique)
 
   pmap_lgl(list(s1, s2), function(ss1, ss2) {
