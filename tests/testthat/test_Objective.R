68cccb915af2847c88840f70bb60ba57492f8f05
be-marc <marcbecker@posteo.de>
2021-10-25 11:43:58 +0200

    adapt unit tests to new checkmate version

--- tests/testthat/test_Objective.R
+++ tests/testthat/test_Objective.R
@@ -223,15 +223,15 @@ test_that("check_values = TRUE with extra output works", {
 
 test_that("assertion on overlapping and reserved names works", {
   expect_error(Objective$new(domain = ps(x = p_lgl()), codomain = ps(x = p_dbl(tags = "maximize"))),
-    regexp = "disjunct from (x).",
+    regexp = "disjunct from {'x'}, but has elements {'x'}",
     fixed = TRUE)
 
   expect_error(Objective$new(domain = ps(batch_nr = p_lgl()), codomain = ps(x = p_dbl(tags = "maximize"))),
-    regexp = "disjunct from (x_domain,timestamp,batch_nr).",
+    regexp = "disjunct from {'x_domain','timestamp','batch_nr'}, but has elements {'batch_nr'}",
     fixed = TRUE)
 
   expect_error(Objective$new(domain = ps(x = p_lgl()), codomain = ps(timestamp = p_dbl(tags = "maximize"))),
-    regexp = "disjunct from (x_domain,timestamp,batch_nr).",
+    regexp = "disjunct from {'x_domain','timestamp','batch_nr'}, but has elements {'timestamp'}",
     fixed = TRUE)
 })
 
@@ -324,5 +324,5 @@ test_that("objective can be initialized with empty codomain", {
   domain = ps(x = p_dbl(lower = -1, upper = 1))
   codomain = ps()
   obj = Objective$new(domain = domain, codomain = codomain)
-  assert_r6(obj, "Objective")
+  expect_r6(obj, "Objective")
 })
