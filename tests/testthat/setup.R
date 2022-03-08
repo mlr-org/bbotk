22f5232453b65ab104fdb7a64b4506145040a7b4
Marc Becker <33069354+be-marc@users.noreply.github.com>
2022-03-08 14:12:17 +0100

    test: disallow partial matching (#177)

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,13 +1,11 @@
-# `irace` package causes partial match warnings
-# fix not on cran yet
-# old_opts = options(
-#   warnPartialMatchArgs = TRUE,
-#   warnPartialMatchAttr = TRUE,
-#   warnPartialMatchDollar = TRUE
-# )
+old_opts = options(
+  warnPartialMatchArgs = TRUE,
+  warnPartialMatchAttr = TRUE,
+  warnPartialMatchDollar = TRUE
+)
 
 # https://github.com/HenrikBengtsson/Wishlist-for-R/issues/88
-# old_opts = lapply(old_opts, function(x) if (is.null(x)) FALSE else x)
+old_opts = lapply(old_opts, function(x) if (is.null(x)) FALSE else x)
 
 old_threshold = lg$threshold
 lg$set_threshold("warn")
