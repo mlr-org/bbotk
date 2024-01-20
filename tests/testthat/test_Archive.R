de27b66395f914a4f248200fcab9e5ecb6c6f993
mb706 <mlr.developer@mb706.com>
2024-01-20 19:48:58 +0100

    adapt merged stuff to new paradox

--- tests/testthat/test_Archive.R
+++ tests/testthat/test_Archive.R
@@ -179,7 +179,12 @@ test_that("best method returns top n results with maximization", {
 
 test_that("best method returns top n results with maximization and ties", {
   codomain = FUN_2D_CODOMAIN
-  codomain$params$y$tags = "maximize"
+  tryCatch({
+    codomain$tags$y = "maximize"
+  }, error = function(e) {
+    # old paradox
+    codomain$params$y$tags = "maximize"
+  })
 
   archive = Archive$new(PS_2D, FUN_2D_CODOMAIN)
   xdt = data.table(x1 = runif(5), x2 = runif(5))
@@ -210,7 +215,12 @@ test_that("best method returns top n results with minimization", {
 
 test_that("best method returns top n results with minimization and ties", {
   codomain = FUN_2D_CODOMAIN
-  codomain$params$y$tags = "minimize"
+  tryCatch({
+    codomain$tags$y = "minimize"
+  }, error = function(e) {
+    # old paradox
+    codomain$params$y$tags = "minimize"
+  })
 
   archive = Archive$new(PS_2D, FUN_2D_CODOMAIN)
   xdt = data.table(x1 = runif(5), x2 = runif(5))
