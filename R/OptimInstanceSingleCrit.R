6aa94b810d4c8c9cd9b054600fc76efb8795c324
be-marc <marcbecker@posteo.de>
2024-05-28 13:25:36 +0200

    refactor: add deprecated message to instances

---
+++ R/OptimInstanceSingleCrit.R
@@ -0,0 +1,39 @@
+#' @title Single Criterion Optimization Instance for Batch Optimization
+#'
+#' @description
+#' `OptimInstanceSingleCrit` is a deprecated class that is now a wrapper around `OptimInstanceBatchSingleCrit`.
+#'
+#' @template param_objective
+#' @template param_search_space
+#' @template param_terminator
+#' @template param_check_values
+#' @template param_callbacks
+#' @template param_keep_evals
+#'
+#' @export
+OptimInstanceSingleCrit = R6Class("OptimInstanceSingleCrit",
+  inherit = OptimInstanceBatchSingleCrit,
+  public = list(
+
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    initialize = function(
+      objective,
+      search_space = NULL,
+      terminator,
+      keep_evals = "all",
+      check_values = TRUE,
+      callbacks = NULL
+      ) {
+
+      message("OptimInstanceSingleCrit is deprecated. Use OptimInstanceBatchSingleCrit instead.")
+
+      super$initialize(
+        objective = objective,
+        search_space = search_space,
+        terminator = terminator,
+        check_values = check_values,
+        callbacks = callbacks)
+    }
+  )
+)
