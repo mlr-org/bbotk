f2b3278526a1358d9b86e9558c7b0de584b3466e
Marc Becker <33069354+be-marc@users.noreply.github.com>
2024-05-28 15:17:54 +0200

    refactor: add deprecated message to instances (#232)

---
+++ R/OptimInstanceMultiCrit.R
@@ -0,0 +1,39 @@
+#' @title Multi Criteria Optimization Instance for Batch Optimization
+#'
+#' @description
+#' `OptimInstanceMultiCrit` is a deprecated class that is now a wrapper around [OptimInstanceBatchMultiCrit].
+#'
+#' @template param_objective
+#' @template param_search_space
+#' @template param_terminator
+#' @template param_check_values
+#' @template param_callbacks
+#' @template param_keep_evals
+#'
+#' @export
+OptimInstanceMultiCrit = R6Class("OptimInstanceMultiCrit",
+  inherit = OptimInstanceBatchMultiCrit,
+  public = list(
+
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    initialize = function(
+      objective,
+      search_space = NULL,
+      terminator,
+      keep_evals = "all",
+      check_values = TRUE,
+      callbacks = NULL
+      ) {
+
+      message("OptimInstanceMultiCrit is deprecated. Use OptimInstanceBatchMultiCrit instead.")
+
+      super$initialize(
+        objective = objective,
+        search_space = search_space,
+        terminator = terminator,
+        check_values = check_values,
+        callbacks = callbacks)
+    }
+  )
+)
