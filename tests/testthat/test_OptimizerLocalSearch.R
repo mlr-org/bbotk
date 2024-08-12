d6cbc3acc4288e1702094a86f8d21fdb0390ba44
Lennart Schneider <lennart.sch@web.de>
2024-08-12 20:06:55 +0200

    ...

--- tests/testthat/test_OptimizerLocalSearch.R
+++ tests/testthat/test_OptimizerLocalSearch.R
@@ -1,5 +1,5 @@
 test_that("OptimizerBatchLocalSearch", {
   z = test_optimizer_1d("local_search", mu = 3L, n_points = 10L, term_evals = 33L)
-  expect_class(z$optimizer, "OptimizerBatchRandomSearch")
+  expect_class(z$optimizer, "OptimizerBatchLocalSearch")
   expect_snapshot(z$optimizer)
 })
