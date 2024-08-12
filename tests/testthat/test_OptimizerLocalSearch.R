7a389e539c4ce7b3446b547c416ca424c2fdedb3
Lennart Schneider <lennart.sch@web.de>
2024-08-12 21:51:49 +0200

    massive speedups (factor of 4-5) by not relying on active bindings of search space

--- tests/testthat/test_OptimizerLocalSearch.R
+++ tests/testthat/test_OptimizerLocalSearch.R
@@ -1,5 +1,6 @@
 test_that("OptimizerBatchLocalSearch", {
-  z = test_optimizer_1d("local_search", mu = 3L, n_points = 10L, term_evals = 33L)
+  z = test_optimizer_1d("local_search", mu = 3L, n_initial_points_random = 100L, n_points = 10L, term_evals = 130L)
   expect_class(z$optimizer, "OptimizerBatchLocalSearch")
   expect_snapshot(z$optimizer)
 })
+
