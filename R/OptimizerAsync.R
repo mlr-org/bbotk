900adb5489fda067bca7382f47f3bb74d4269ddc
be-marc <marcbecker@posteo.de>
2024-12-09 13:01:11 +0100

    ...

--- R/OptimizerAsync.R
+++ R/OptimizerAsync.R
@@ -90,7 +90,7 @@ optimize_async_default = function(instance, optimizer, design = NULL, n_workers 
       rush$start_remote_workers(
         worker_loop = bbotk_worker_loop,
         packages = c(optimizer$packages, instance$objective$packages, "bbotk"),
-        wait_for_workers = TRUE,
+        wait_for_workers = FALSE,
         optimizer = optimizer,
         instance = instance)
     } else if (rush::rush_available()) {
