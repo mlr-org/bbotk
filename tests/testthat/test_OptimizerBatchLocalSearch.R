ba930229a0d7b68dc5cb8022a4c8bb3757fad3c0
be-marc <marcbecker@posteo.de>
2025-10-08 16:05:34 +0200

    tests: local search

--- tests/testthat/test_OptimizerBatchLocalSearch.R
+++ tests/testthat/test_OptimizerBatchLocalSearch.R
@@ -227,7 +227,7 @@ test_that("OptimizerBatchLocalSearch works with dependencies on factor parameter
   expect_character(instance$archive$data$x2, any.missing = FALSE)
   if (nrow(instance$archive$data[x2 == "c"])) expect_set_equal(instance$archive$data[x2 == "c"]$y, 3)
   if (nrow(instance$archive$data[x2 == "c"])) expect_set_equal(instance$archive$data[x2 == "c"]$x1, NA)
-  expect_true(abs(instance$archive$best()$y) < 1e-3)
+  expect_true(abs(instance$archive$best()$y) < 1e-2)
 })
 
 test_that("OptimizerBatchLocalSearch works with dependencies on numeric parameters", {
